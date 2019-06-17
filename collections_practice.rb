92d6be48b3c81e4039f557767697ebba684ac54e
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-17 23:22:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  array =  [25, 7, 1]
+   array.sort
+end
 
+def sort_array_desc(array)
+  array = [25, 7, 14]
+    array.sort do |a, b|
+      b <=> a 
+    end 
+  end
+  
+  def sort_array_char_count(array)
+    array = ["dogs", "cat", "Horses"]
+      array.sort do |a, c|
+        a <=> c
+    end  
+    # double check this one ^
+    
+    def swap_elements(array)
+      
+    
+    
+    
